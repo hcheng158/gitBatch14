149d501ccd20c56ddac05d9c6dc2458ba6bab287
hcheng158 <hcheng158@yahoo.com>
2022-09-27 17:10:46 -0700

    Third commit yo

--- src/Class03.java
+++ src/Class03.java
@@ -3,8 +3,8 @@ public class Class03 {
 
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
-		System.out.println("changes 1");
-
+		System.out.println("changes 2");
+		System.out.println(" Changes 3");
 	}
 
 }
